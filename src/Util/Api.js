da1e69e0a0a70f54c6088bcd94d4dfe0e5443050
Everton Gabriel Zoelner <everton.zoelner@gmail.com>
2018-01-29 20:09:04 -0200

    Funcional

--- src/Util/Api.js
+++
@@ -1,6 +0,0 @@
-export function fetchData(url) {
-    return fetch(`http://localhost:3001/${url}`, { headers: {
-        'Authorization': 'Everton Zoelner',
-        'Method': 'GET'
-    }})
-}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
-import registerServiceWorker from './registerServiceWorker';
 import { createStore, applyMiddleware, compose } from 'redux';
-import reducer from './Reducers/index';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
+import registerServiceWorker from './registerServiceWorker';
+import reducer from './Reducers/index';
+
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
